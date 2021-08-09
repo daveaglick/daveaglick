40dc62cbb903dd084c17880409e5af242980d0a3
Dave Glick <dave@daveaglick.com>
2021-08-09 12:06:45 -0400

    Whops, wrong Netlify ID

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace daveaglick
             await Bootstrapper
                 .Factory
                 .CreateWeb(args)
-                .DeployToNetlify("daveaglick", Config.FromSetting<string>("NETLIFY_TOKEN"))
+                .DeployToNetlify("9bef82cf-1731-4cf0-ab3d-693ab0754810", Config.FromSetting<string>("NETLIFY_TOKEN"))
                 .RunAsync();
     }
 }
